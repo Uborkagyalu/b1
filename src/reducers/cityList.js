4f2d0d9abc48454c93db37f3d3e70bb3a1eef912
Uborkagyalu <herbent.gergo@gmail.com>
2021-12-12 18:47:06 +0100

    1

--- src/reducers/cityList.js
+++ src/reducers/cityList.js
@@ -1,7 +1,9 @@
-const cityListReducer = (state = [{name: "Budapest"}], action) => {
+const cityListReducer = (state = JSON.parse(localStorage.getItem("localCityList")) || [{name: "Budapest"}], action) => {
     switch(action.type) {
         case 'addcity': {
-            return [...state, {name: action.target}];
+            state = [...state, {name: action.target}];
+            localStorage.setItem("localCityList", JSON.stringify(state));
+            return state;
         }
         default:
             return state;
